35ac04d0150c81e4fc6cbaa5eccb3816e523fe7a
Lincoln Sanderson <lincoln@paperton.com>
2018-03-10 17:56:11 +0100

    Scopes apparently changed...

--- lib/paperclip/storage/database.rb
+++ lib/paperclip/storage/database.rb
@@ -98,10 +98,21 @@ module Paperclip
         if @attachment_class.const_defined?(class_name, false)
           @paperclip_file_model = @attachment_class.const_get(class_name, false)
         else
-          @paperclip_file_model = @attachment_class.const_set(class_name, Class.new(::ActiveRecord::Base))
-          @paperclip_file_model.table_name = @options[:database_table] || name.to_s.pluralize
-          @paperclip_file_model.validates_uniqueness_of :style, :scope => instance.class.table_name.classify.underscore + '_id'
-          @paperclip_file_model.scope :file_for, lambda {|style| @paperclip_file_model.where('style = ?', style) }
+          case ActiveModel::VERSION::MAJOR
+          when 3
+          when 4
+            @paperclip_file_model = @attachment_class.const_set(class_name, Class.new(::ActiveRecord::Base))
+            @paperclip_file_model.table_name = @options[:database_table] || name.to_s.pluralize
+            @paperclip_file_model.validates_uniqueness_of :style, :scope => instance.class.table_name.classify.underscore + '_id'
+            @paperclip_file_model.scope :file_for, lambda {|style| @paperclip_file_model.where('style = ?', style) }
+          when 5
+            @paperclip_file_model = @attachment_class.const_set(class_name, Class.new(::ApplicationRecord))
+            @paperclip_file_model.table_name = @options[:database_table] || name.to_s.pluralize
+            @paperclip_file_model.validates_uniqueness_of :style, :scope => instance.class.table_name.classify.underscore + '_id'
+            @paperclip_file_model.scope :file_for, lambda {|style| where(:style => style) }
+          else
+            raise "ActiveModel version #{ActiveModel::VERSION::STRING} is not supported (yet)"
+          end
         end
       end
       private :setup_paperclip_file_model
